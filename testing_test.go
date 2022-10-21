c2392a6bfa85ad4d2b3b78277a0404d27b491ef1
Henry Snopek <hhsnopek@gmail.com>
2022-10-21 14:00:05 -0500

    feat: add SkipIfProviderIsNotHealthyWithContext

--- testing_test.go
+++ testing_test.go
@@ -1,9 +1,14 @@
 package testcontainers
 
 import (
+	"context"
 	"testing"
 )
 
 func ExampleSkipIfProviderIsNotHealthy() {
 	SkipIfProviderIsNotHealthy(&testing.T{})
+}
+
+func ExampleSkipIfProviderIsNotHealthyWithContext() {
+	SkipIfProviderIsNotHealthyWithContext(&testing.T{}, context.Background())
 }
