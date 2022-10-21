70b62d92bbb6ab8e3af86a6bf6f9e7b9247b3031
Henry Snopek <hhsnopek@gmail.com>
2022-10-21 14:00:05 -0500

    feat: add SkipIfProviderIsNotHealthyWithContext

--- testing.go
+++ testing.go
@@ -5,12 +5,9 @@ import (
 	"testing"
 )
 
-// SkipIfProviderIsNotHealthy is a utility function capable of skipping tests
-// if the provider is not healthy, or running at all.
-// This is a function designed to be used in your test, when Docker is not mandatory for CI/CD.
-// In this way tests that depend on Testcontainers won't run if the provider is provisioned correctly.
-func SkipIfProviderIsNotHealthy(tb testing.TB) {
-	ctx := context.Background()
+// SkipIfProviderIsNotHealthWithContext is a context configurable varient of
+// SkipIfProviderIsNotHealthy
+func SkipIfProviderIsNotHealthyWithContext(tb testing.TB, ctx context.Context) {
 	provider, err := ProviderDocker.GetProvider()
 	if err != nil {
 		tb.Skipf("Docker is not running. TestContainers can't perform this work without it: %s", err)
@@ -20,6 +17,14 @@ func SkipIfProviderIsNotHealthy(tb testing.TB) {
 	if err != nil {
 		tb.Skipf("Docker is not running. TestContainers can't perform this work without it: %s", err)
 	}
+}
+
+// SkipIfProviderIsNotHealthy is a utility function capable of skipping tests
+// if the provider is not healthy, or running at all.
+// This is a function designed to be used in your test, when Docker is not mandatory for CI/CD.
+// In this way tests that depend on Testcontainers won't run if the provider is provisioned correctly.
+func SkipIfProviderIsNotHealthy(tb testing.TB) {
+	SkipIfProviderIsNotHealthyWithContext(tb, context.Background())
 }
 
 // CleanupContainer terminates the passed container after testing.TB has
