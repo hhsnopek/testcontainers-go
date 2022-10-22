fa13a5eacbea235c64843f36bd87eebd9dbb97c6
Henry Snopek <hhsnopek@gmail.com>
2022-10-22 00:08:58 -0500

    feat: add MultiStrategy#WithDeadline
    deprecate: MultiStrategy#WithStartupTimeout

--- wait/all.go
+++ wait/all.go
@@ -12,13 +12,21 @@ var _ Strategy = (*MultiStrategy)(nil)
 type MultiStrategy struct {
 	// all Strategies should have a startupTimeout to avoid waiting infinitely
 	startupTimeout time.Duration
+	deadline       *time.Duration
 
 	// additional properties
 	Strategies []Strategy
 }
 
+// Deprecated: use WithDeadline
 func (ms *MultiStrategy) WithStartupTimeout(startupTimeout time.Duration) *MultiStrategy {
 	ms.startupTimeout = startupTimeout
+	return ms
+}
+
+// WithDeadline sets a time.Duration which limits all wait strategies
+func (ms *MultiStrategy) WithDeadline(deadline time.Duration) *MultiStrategy {
+	ms.deadline = &deadline
 	return ms
 }
 
@@ -30,8 +38,11 @@ func ForAll(strategies ...Strategy) *MultiStrategy {
 }
 
 func (ms *MultiStrategy) WaitUntilReady(ctx context.Context, target StrategyTarget) (err error) {
-	ctx, cancelContext := context.WithTimeout(ctx, ms.startupTimeout)
-	defer cancelContext()
+	var cancel context.CancelFunc
+	if ms.deadline != nil {
+		ctx, cancel = context.WithTimeout(ctx, *ms.deadline)
+		defer cancel()
+	}
 
 	if len(ms.Strategies) == 0 {
 		return fmt.Errorf("no wait strategy supplied")
